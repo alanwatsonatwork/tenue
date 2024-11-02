4388defe6c70045988dfaf1b526afc478e1f7ca4
Alan Watson <alan@astro.unam.mx>
2024-11-02 17:05:46 -0600

    Use .fits.fz files if no .fits files are found.

--- tenue/fits.py
+++ tenue/fits.py
@@ -4,18 +4,32 @@ import astropy.io.fits
 ihdu = 0
 
 
+def _ihdu(fitspath):
+    """
+    Return the HDU index for the actual data in a FITS file. This is 0
+    for an uncompressed FITS file and 1 for a fpack-compressed FITS
+    file. The suffix of the fitspath is used to determine if the file is
+    compressed or not; if the suffix is ".fz", the file is assumed to be
+    compressed.
+    """
+    if fitspath[-3:] == ".fz":
+        return 1
+    else:
+        return 0
+
+
 def readrawheader(fitspath, name=None):
     if name is not None:
         print(
             "%s: reading header from raw file %s." % (name, os.path.basename(fitspath))
         )
     hdu = astropy.io.fits.open(fitspath)
-    return hdu[ihdu].header
+    return hdu[_ihdu(fitspath)].header
 
 
 def readrawdata(fitspath, name=None):
     if name is not None:
         print("%s: reading data from raw file %s." % (name, os.path.basename(fitspath)))
     hdu = astropy.io.fits.open(fitspath)
-    return np.array(hdu[ihdu].data, dtype=np.float32)
+    return np.array(hdu[_ihdu(fitspath)].data, dtype=np.float32)
 
