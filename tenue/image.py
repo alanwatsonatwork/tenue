294e7b73b4717850b0ce1da6280d4542ad8e91bb
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2024-11-03 18:51:52 -0600

    Report estimated noise in bias, dark, and flat.

--- tenue/image.py
+++ tenue/image.py
@@ -8,6 +8,7 @@ import astropy.visualization
 
 import matplotlib.pyplot as plt
 
+
 def clippedmean(stack, sigma=3.0, axis=None):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", Warning)
@@ -26,6 +27,15 @@ def clippedsigma(stack, sigma=3.0, axis=None):
     return sigma
 
 
+def clippedmeanandsigma(stack, sigma=3.0, axis=None):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", Warning)
+        mean, median, sigma = astropy.stats.sigma_clipped_stats(
+            np.array(stack), sigma=sigma, axis=axis, cenfunc="median", stdfunc="mad_std"
+        )
+    return mean, sigma
+
+
 def show(data, zrange=False, zscale=False, contrast=0.25):
 
     if zrange:
@@ -34,11 +44,13 @@ def show(data, zrange=False, zscale=False, contrast=0.25):
     else:
         interval = astropy.visualization.ZScaleInterval(contrast=contrast)
         stretch = astropy.visualization.LinearStretch()
-    norm = astropy.visualization.ImageNormalize(data, interval=interval, stretch=stretch)
-    
+    norm = astropy.visualization.ImageNormalize(
+        data, interval=interval, stretch=stretch
+    )
+
     tickinterval = int(math.pow(2, int(math.log2(np.max(data.shape) / 8))))
     ticks = list(range(0, np.max(data.shape), tickinterval))
-    
+
     plt.figure(figsize=(10, 10))
     plt.imshow(data, origin="lower", norm=norm)
     plt.xticks(ticks)
