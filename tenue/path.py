acfb6e0f8955832d835c6d76f515479fada6c26c
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2024-11-04 19:26:19 -0600

    Use exposure times for darks.

--- tenue/path.py
+++ tenue/path.py
@@ -5,17 +5,23 @@ import tenue.fits
 import tenue.instrument
 
 
-def getrawfitspaths(directorypath, filter=None):
+def getrawfitspaths(directorypath, filter=None, exposuretime=None):
     fitspaths = glob.glob(directorypath + "/*.fits")
     if len(fitspaths) == 0:
         fitspaths = glob.glob(directorypath + "/*.fits.fz")
     fitspaths = sorted(fitspaths)
-    if filter == None:
-        return fitspaths
-    else:
-        return list(
+    if filter != None:
+        fitspaths = list(
             fitspath
             for fitspath in fitspaths
             if tenue.fits.readrawheader(fitspath)[tenue.instrument.filterkeyword()]
             == filter
         )
+    if exposuretime != None:
+        fitspaths = list(
+            fitspath
+            for fitspath in fitspaths
+            if tenue.fits.readrawheader(fitspath)[tenue.instrument.exposuretimekeyword()]
+            == exposuretime
+        )
+    return fitspaths
