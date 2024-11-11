9d7b6271f9854d9e4375f5aa13d6db08a53d7952
Alan Watson <alan@astro.unam.mx>
2024-11-11 07:16:12 -0600

    Update object.py

--- tenue/object.py
+++ tenue/object.py
@@ -164,7 +164,7 @@ def makeobject(
     dxlist = []
     dylist = []
 
-    for header in headerlist:
+    for header, fitspath in zip(headerlist, fitspathlist):
 
         alpha = math.radians(tenue.instrument.alpha(header))
         delta = math.radians(tenue.instrument.delta(header))
@@ -175,7 +175,7 @@ def makeobject(
         ddelta = (delta - refdelta) / pixelscale
         dx = +int(np.round(dalpha * math.cos(rotation) - ddelta * math.sin(rotation)))
         dy = -int(np.round(dalpha * math.sin(rotation) + ddelta * math.cos(rotation)))
-        print("makeobject: raw offset is dx = %+3d px dy = %+3d px." % (dx, dy))
+        print("makeobject: %s: raw offset is dx = %+3d px dy = %+3d px." % (fitspath, dx, dy))
 
         dxlist.append(dx)
         dylist.append(dy)
