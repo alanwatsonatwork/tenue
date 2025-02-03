99ab234077c3776ef3fcbdd1224d62e256f05064
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2025-02-03 17:20:28 -0600

    Added ddragoblue instrument.

---
+++ tenue/instrument/ddragoblue.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import tenue.instrument
+
+
+def overscanyslice(header):
+    binning = int(header["SDTBN"])
+    return slice(int(0 / binning), int(4096 / binning))
+
+
+def overscanxslice(header):
+    binning = int(header["SDTBN"])
+    return slice(int(0 / binning), int(48 / binning))
+
+
+def trimyslice(header):
+    binning = int(header["SDTBN"])
+    return slice(int(0 / binning), int(4096 / binning))
+
+
+def trimxslice(header):
+    binning = int(header["SDTBN"])
+    return slice(int(48 / binning), int(4144 / binning))
+
+
+def dorotate(header, data):
+    rotation = header["SMTMRO"]
+    return np.rot90(data, -int(rotation / 90))
+
+
+def alpha(header):
+    return header["SMTMRA"]
+
+
+def delta(header):
+    return header["SMTMDE"]
+
+
+def rotation(header):
+    date = header["DATE-OBS"][:10]
+    return 0
+
+
+def pixelscale(header):
+    binning = int(header["SDTBN"])
+    return 0.38 * binning / 3600
+
+
+tenue.instrument.setvalues(
+    overscanxslice=overscanxslice,
+    overscanyslice=overscanyslice,
+    trimxslice=trimxslice,
+    trimyslice=trimyslice,
+    dorotate=dorotate,
+    rotation=rotation,
+    alpha=alpha,
+    delta=delta,
+    pixelscale=pixelscale,
+)
