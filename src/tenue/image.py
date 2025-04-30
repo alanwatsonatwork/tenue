60c469e0739b61d5775d87333beb65f733998043
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2025-04-30 12:43:38 -0600

    Document sigma_clipped_stats

--- src/tenue/image.py
+++ src/tenue/image.py
@@ -10,30 +10,24 @@ import scipy.ndimage
 
 import matplotlib.pyplot as plt
 
-def sigma_clipped_stats(data, sigma=3.0, axis=None):
+def sigma_clipped_stats(stack, sigma=3.0, axis=None):
     """
-    Return sigma-clipped statistics of the data.
+    Return sigma-clipped statistics of a stack of data.
 
-    This function behaves exactly as:
+    This behaves exactly as:
     
         astropy.stats.sigma_clipped_stats(
-            data, sigma=sigma, axis=axis, cenfunc="median", stdfunc="mad_std"
+            stack, sigma=sigma, axis=axis, cenfunc="median", stdfunc="mad_std"
         )
 
-    except that it converts any arrays to float32 before returning them.
-
-    Furthermore, for the common case of clipping a stack of 2D arrays, it is
-    does so row by row, which is much more efficient in terms of memory use.
-
-    :param data: A data of data
-    :param sigma: The number of standard deviations for the upper and lower
-        clipping limits. Defaults to 3.0
+    :param stack: A stack of data
+    :param sigma: The number of standard deviations for the upper and lower clipping limits. Defaults to 3.0
     :param axis: The axis along with to clip the data. Defaults to None.
     :return: The mean, median, and standard deviation of the data.
     """
 
-    if not isinstance(data, np.ndarray):
-        data = np.array(data)
+    if not isinstance(stack, np.ndarray):
+        stack = np.array(stack)
 
     with warnings.catch_warnings():
 
